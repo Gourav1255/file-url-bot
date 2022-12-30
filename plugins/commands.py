34910d5146bb4353ad67313e77ce311ba723fca9
Gourav1255 <114558061+Gourav1255@users.noreply.github.com>
2022-12-30 12:36:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
-                InlineKeyboardButton('🔔 Request Movies 🤖', url='https://t.me/requestluciferdownload')
+                InlineKeyboardButton('🔔 Request Movies 🤖', url='https://t.me/HDMOVIEAS143')
             ],
             [
                 InlineKeyboardButton('🙆🏻 Help 🦾', url=f"https://t.me/{temp.U_NAME}?start=help"),
@@ -31,7 +31,7 @@ async def start(client, message):
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
-        await asyncio.sleep(2) # 😢 https://github.com/SohanRazz/All-rounder-search-bot/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
+        await asyncio.sleep(2) # 😢 https://github.com/Gourav1255/file-url-bot/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
@@ -44,8 +44,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕↖️ Add Me To Your Groups ↗️➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🧞‍♀️ Search 🧐', url='https://t.me/+InVosekxdxFkNmVl'),
-            InlineKeyboardButton('🔔 Request Movies 🤖', url='https://t.me/requestluciferdownload')
+            InlineKeyboardButton('🧞‍♀️ Search 🧐', url='https://t.me/HDMOVIEAS143'),
+            InlineKeyboardButton('🔔 Request Movies 🤖', url='https://t.me/HDMOVIEAS143')
             ],[
             InlineKeyboardButton('🙆🏻 Help 🦾', callback_data='help'),
             InlineKeyboardButton('♥️ About ♥️', callback_data='about')
@@ -91,7 +91,7 @@ async def start(client, message):
             InlineKeyboardButton('➕↖️ Add Me To Your Groups ↗️➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🧞‍♀️ Search 🧐', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🔔 Request Movies 🤖', url='https://t.me/requestluciferdownload')
+            InlineKeyboardButton('🔔 Request Movies 🤖', url='https://t.me/HDMOVIEAS143')
             ],[
             InlineKeyboardButton('🙆🏻 Help 🦾', callback_data='help'),
             InlineKeyboardButton('♥️ About ♥️', callback_data='about')
@@ -360,7 +360,7 @@ async def delete_all_index(bot, message):
 @Client.on_callback_query(filters.regex(r'^autofilter_delete'))
 async def delete_all_index_confirm(bot, message):
     await Media.collection.drop()
-    await message.answer('♥️ Thank You SohanRajpurohit ♥️')
+    await message.answer('♥️ Thank You SHARMAJI ♥️')
     await message.message.edit('Succesfully Deleted All The Indexed Files.')
 
 
